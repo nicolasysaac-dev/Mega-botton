c603e1db8a75447d2436b04b827e95755faf02f0
nicolasysaac-dev <nicolas.ysaac@gmail.com>
2026-07-16 15:11:26 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let cliques = Number(localStorage.getItem("cliques")) || 0;
 const contador = document.getElementById("contador");
 const botao = document.getElementById("botao");
 
+const abrirLoja = document.getElementById("abrirLoja");
+const fecharLoja = document.getElementById("fecharLoja");
+const loja = document.getElementById("loja");
+
 function atualizar() {
     contador.textContent = "Cliques: " + cliques;
 }
@@ -17,4 +21,13 @@ botao.addEventListener("click", function () {
 
     localStorage.setItem("cliques", cliques);
 
+});
+
+
+abrirLoja.addEventListener("click", function () {
+    loja.style.display = "block";
+});
+
+fecharLoja.addEventListener("click", function () {
+    loja.style.display = "none";
 });
